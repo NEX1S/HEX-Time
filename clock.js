6985af046a0eef94ae825191f7ded6595f4add56
Michael Unterthurner <mitchjem555@hotmail.com>
2020-10-02 20:34:22 +0200

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,9 +1,5 @@
 setInterval(function() {
     //var hex = '#' + new Date().toISOString().substr(11, 8).replace(/:/g, '');
-    
-    var hex = '#' + (currentdate.getHours() + currentdate.getMinutes() + currentdate.getSeconds()).toISOString().substr(11, 8).replace(/:/g, '');
-
-    
     document.getElementById('clock').innerHTML = hex;
     document.body.style.backgroundColor = hex;
 }, 500)
